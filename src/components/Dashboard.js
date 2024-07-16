466ec01bdf43367df129f98cc151d0a089003cd4
AnwarBaig <anwarmoghal13@gmail.com>
2024-07-16 12:37:54 +0530

    addedNav

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-function Dashboard() {
+// import "./DashboardPage.css";
+import "./styles.css";
+function DashboardPage() {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
 
@@ -9,7 +10,7 @@ function Dashboard() {
     const storedUser = JSON.parse(localStorage.getItem("user"));
 
     if (!storedUser) {
-      navigate("/login");
+      navigate("/loginpage");
     } else {
       setUser(storedUser);
     }
@@ -17,22 +18,45 @@ function Dashboard() {
 
   const handleLogout = () => {
     localStorage.removeItem("user");
-    navigate("/login");
+    navigate("/loginpage");
   };
 
   if (!user) {
     return null;
   }
-
   return (
-    <div className="container">
-      <h2>Dashboard</h2>
-      <p>Welcome, {user.user_firstname}!</p>
-      <p>Email: {user.user_email}</p>
-      <p>Phone: {user.user_phone}</p>
-      <button onClick={handleLogout}>Logout</button>
+    <div>
+      <div className="nav-container">
+        <nav className="navbar">
+          <div>
+            <img
+              src="https://res.cloudinary.com/dgkru5vsg/image/upload/v1721069534/logo-color_flebfb.png"
+              className="Logo"
+              alt=""
+            />
+          </div>
+          <div>
+            <ul className="navbar-list">
+              <li className="navbar-item">Home</li>
+              <li className="navbar-item">Dashboard</li>
+              <li className="navbar-item">Services</li>
+              <li className="navbar-item">About</li>
+              <li className="navbar-item">Contact</li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <h1 className="heading">Transform. Accelerate. Scale. Optimize.</h1>
+      <div className="dashboard-container">
+        <div className="dashboard">
+          <h1>Dashboard</h1>
+          <p>Welcome, {user.user_firstname}!</p>
+          <p>Email: {user.user_email}</p>
+          <p>Phone: {user.user_phone}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default Dashboard;
+export default DashboardPage;
